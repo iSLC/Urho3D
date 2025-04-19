910fd2b5587bcbfc57df0789ec4114ded20219dd
Sandu Liviu Catalin <slc.universe@gmail.com>
2025-04-19 19:37:33 +0300

    Update TypeUtils.h

--- Source/Urho3D/Base/TypeUtils.h
+++ Source/Urho3D/Base/TypeUtils.h
@@ -8,14 +8,60 @@
 
 namespace Urho3D {
 
+
+/*
+ * Forward declarations.
+*/
+struct StrView;
+template < class, size_t > struct Span;
+template < class, size_t > struct Array;
+
+/*
+ * Forward type-traits declarations.
+*/
+template < class > struct IsSpan;
+template < class > struct IsArray;
+template < class > struct IsStrView;
 template < class, class = void > struct HasSizeAndData;
+
+/// Helper of \ref IsSpan used to avoid having to explicitly accessing the `value` member.
+template < class T > inline constexpr auto IsSpan_v = IsSpan< T >::value;
+/// Helper of \ref IsArray used to avoid having to explicitly access the `value` member.
+template < class T > inline constexpr auto IsArray_v = IsArray< T >::value;
+/// Helper of \ref IsStrView used to avoid having to explicitly accessing the `value` member.
+template < class T > inline constexpr auto IsStrView_v = IsStrView< T >::value;
 
 /// Helper of \ref HasSizeAndData used to avoid having to explicitly accessing the `value` member.
 template < class T > inline constexpr auto HasSizeAndData_v = HasSizeAndData< T >::value;
 
-/// Check whether `T` is a type that has `Size()` and `Data()` member methods. \remark Not a standard feature!
-template < class, class > struct HasSizeAndData : std::false_type { };
-/// Partial specialization for the case when `T` is actually has `Size()` and `Data()` member methods.
-template < class T > struct HasSizeAndData< T, std::void_t< decltype(Size(declval< T >())), decltype(Data(declval< T >())) > > : std::true_type { };
+/// Check whether `T` is a \ref Span type.
+template < class > struct IsSpan : std::false_type { };
+/// Partial specialization for the case when `T` is a \ref Span type.
+template < class T, size_t S > struct IsSpan< Span< T, S > > : std::true_type { };
+
+/// Check whether `T` is an Array container.
+template < class T > struct IsArray : public std::false_type { };
+/// Partial specialization for the case when `T` is an Array container.
+template < class T, size_t N > struct IsArray< Array< T, N > > : public std::true_type { };
+
+/// Check whether `T` is a \ref StrView type.
+template < class > struct IsStrView : std::false_type { };
+/// Partial specialization for the case when `T` is a \ref StrView type.
+template < > struct IsStrView< StrView > : std::true_type { };
+
+namespace Concept {
+    /// Constrains \p T to be a \p Span type.
+    template < class T > concept DataView = IsSpan_v< T >;
+    /// Constrains \p T to be a \p StrView type.
+    template < class T > concept CharView = IsStrView_v< T >;
+    /// Constrains \p T to be a \p Array type.
+    template < class T > concept ArrayContainer = IsArray_v< T >;
+    /// Constrains \p T to implement Data(c) and Size(c) functionality. 
+    template < class T > concept DataAndSize = requires(T c) {
+        { Data(c) } -> std::contiguous_iterator;
+        { Size(c) } -> std::integral;
+    };
+} // Namespace:: Concept
+
 
 } // Namespace:: Urho3D
