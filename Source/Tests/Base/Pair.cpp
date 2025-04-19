9b2064c787733ebfce0cff36c895b5f05df93a0b
Sandu Liviu Catalin <slc.universe@gmail.com>
2025-04-19 19:37:25 +0300

    Update Pair.cpp

--- Source/Tests/Base/Pair.cpp
+++ Source/Tests/Base/Pair.cpp
@@ -4,4 +4,4 @@ DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_BEGIN
 #include <type_traits>
 DOCTEST_MAKE_STD_HEADERS_CLEAN_FROM_WARNINGS_ON_WALL_END
 
-//#include <Urho3D/Base/Pair.h>
+#include <Urho3D/Base/Pair.h>
